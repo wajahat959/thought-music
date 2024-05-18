33ef3a2ce14e134f68b8ec37af546bd6f10c5bd1
wajahat959 <wajahat.h@anemoia.dev>
2024-05-18 11:04:04 +0500

    selfassessment changed

--- components/Screens/Home/SelfAssessment.js
+++ components/Screens/Home/SelfAssessment.js
@@ -1,21 +1,84 @@
-import Header from '@/components/global/Header';
-import Background from '@/components/global/ImageBackground';
-import React, { useState } from 'react';
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import Button from '../../global/Button';
+import Header from "@/components/global/Header";
+import Background from "@/components/global/ImageBackground";
+import React, { useState } from "react";
+import {
+  Modal,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Button from "../../global/Button";
 
 const SelfAssessment = () => {
   const [answers, setAnswers] = useState(Array(7).fill(null));
-  const [resultText, setResultText] = useState('');
-
+  const [resultText, setResultText] = useState("");
+  const [isPlaylistVisible, setIsPlaylistVisible] = useState(false);
   const questions = [
-    { question: 'Feelings nervous, anxious or on edge', options: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'] },
-    { question: 'Not being able to stop or control worrying', options: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'] },
-    { question: 'Worrying too much about different things', options: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'] },
-    { question: 'Trouble relaxing', options: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'] },
-    { question: 'Being so restless that it is hard to sit still', options: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'] },
-    { question: 'Becoming easily annoyed or irritable', options: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'] },
-    { question: 'Feeling afraid as if something awful might happen', options: ['Not at all', 'Several days', 'More than half the days', 'Nearly every day'] },
+    {
+      question: "Feelings nervous, anxious or on edge",
+      options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every day",
+      ],
+    },
+    {
+      question: "Not being able to stop or control worrying",
+      options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every day",
+      ],
+    },
+    {
+      question: "Worrying too much about different things",
+      options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every day",
+      ],
+    },
+    {
+      question: "Trouble relaxing",
+      options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every day",
+      ],
+    },
+    {
+      question: "Being so restless that it is hard to sit still",
+      options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every day",
+      ],
+    },
+    {
+      question: "Becoming easily annoyed or irritable",
+      options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every day",
+      ],
+    },
+    {
+      question: "Feeling afraid as if something awful might happen",
+      options: [
+        "Not at all",
+        "Several days",
+        "More than half the days",
+        "Nearly every day",
+      ],
+    },
   ];
 
   const handleSelectOption = (questionIndex, optionIndex) => {
@@ -26,75 +89,147 @@ const SelfAssessment = () => {
 
   const calculateTotal = () => {
     if (answers.includes(null)) {
-      alert('Please answer all questions.');
+      alert("Please answer all questions.");
       return;
     }
 
     const total = answers.reduce((acc, curr) => acc + curr, 0);
-
     if (total >= 0 && total <= 4) {
-      setResultText('Minimal Anxiety');
+      setResultText("Minimal Anxiety");
     } else if (total >= 5 && total <= 9) {
-      setResultText('Mild Anxiety');
+      setResultText("Mild Anxiety");
     } else if (total >= 10 && total <= 14) {
-      setResultText('Moderate Anxiety');
+      setResultText("Moderate Anxiety");
     } else if (total >= 15 && total <= 21) {
-      setResultText('Severe Anxiety');
+      setResultText("Severe Anxiety");
     }
+
+    setIsPlaylistVisible(true);
   };
 
   const resetAssessment = () => {
     setAnswers(Array(7).fill(null));
-    setResultText('');
+    setResultText("");
+  };
+  const onClose = (index) => {
+    setIsPlaylistVisible(false);
+  };
+  const onOpen = (index) => {
+    setIsPlaylistVisible(true);
   };
 
   return (
-  <Header>
-     <Background>
-      <ScrollView>
-        <View style={styles.container}>
-          <Text style={styles.header}>GAD-7 Anxiety</Text>
-          {questions.map((questionObj, index) => (
-            <View key={index} style={styles.card}>
-              <Text style={styles.questionText}>{questionObj.question}</Text>
-              {questionObj.options.map((option, optionIndex) => (
-                <TouchableOpacity
-                  key={optionIndex}
-                  onPress={() => handleSelectOption(index, optionIndex)}
-                  style={[
-                    styles.optionContainer,
-                    answers[index] === optionIndex ? styles.selectedOption : {},
-                  ]}
-                >
-                  <Text style={styles.optionText}>{option}</Text>
-                  <View
-                    style={{
-                      width: 20,
-                      height: 20,
-                      borderRadius: 10,
-                      borderWidth: 1,
-                      marginLeft: 10,
-                      backgroundColor: answers[index] === optionIndex ? 'orange' : 'white',
-                    }}
-                  />
-                </TouchableOpacity>
-              ))}
-            </View>
-          ))}
-          {resultText !== '' &&<View style={{flexDirection:'row',alignSelf:'center'}}>
+    <Header>
+      <Background>
+        <ScrollView>
+          <View style={styles.container}>
+            <Text style={styles.header}>GAD-7 Anxiety</Text>
+            {questions.map((questionObj, index) => (
+              <View key={index} style={styles.card}>
+                <Text style={styles.questionText}>{questionObj.question}</Text>
+                {questionObj.options.map((option, optionIndex) => (
+                  <TouchableOpacity
+                    key={optionIndex}
+                    onPress={() => handleSelectOption(index, optionIndex)}
+                    style={[
+                      styles.optionContainer,
+                      answers[index] === optionIndex
+                        ? styles.selectedOption
+                        : {},
+                    ]}
+                  >
+                    <Text style={styles.optionText}>{option}</Text>
+                    <View
+                      style={{
+                        width: 20,
+                        height: 20,
+                        borderRadius: 10,
+                        borderWidth: 1,
+                        marginLeft: 10,
+                        backgroundColor:
+                          answers[index] === optionIndex ? "orange" : "white",
+                      }}
+                    />
+                  </TouchableOpacity>
+                ))}
+              </View>
+            ))}
+            {/* {resultText !== '' &&<View style={{flexDirection:'row',alignSelf:'center'}}>
           <Text style={styles.averageText}>GAD-7 Anxiety Severity:</Text>
           <Text style={styles.result}>{resultText}</Text>
-            </View>}
-          <Button buttonType="login" onPress={calculateTotal} buttonStyles={styles.button}>
-            Calculate
-          </Button>
-          <Button buttonType="login" onPress={resetAssessment} buttonStyles={styles.button}>
-            Reset
-          </Button>
-        </View>
-      </ScrollView>
-     </Background>
-      </Header>
+            </View>} */}
+            <Button
+              buttonType="login"
+              onPress={() => {
+                calculateTotal();
+          
+              }}
+              buttonStyles={styles.button}
+            >
+              Submit
+            </Button>
+            <Button
+              buttonType="login"
+              onPress={resetAssessment}
+              buttonStyles={styles.button}
+            >
+              Reset
+            </Button>
+          </View>
+        </ScrollView>
+      </Background>
+
+      <Modal
+        visible={isPlaylistVisible}
+        animationType="fade"
+        transparent={true}
+        onRequestClose={() => {}}
+      >
+        <Header>
+          <Background>
+            <View style={styles.modalContainer}>
+              <View style={styles.card2}>
+                <View style={{ flexDirection: "row",justifyContent:'space-between'}}>
+                  <Text style={styles.text2}>
+                    GAD-7 Anxiety Severity:
+                  </Text>
+                  <Text style={styles.text}>{resultText}</Text>
+                </View>
+              </View>
+
+              <View>
+                <TouchableOpacity
+                  onPress={() => {onClose(); resetAssessment()}}
+                  style={{
+                    borderWidth: 1,
+                    backgroundColor: "#7339BA",
+                    opacity: 0.6,
+                    borderRadius: 20,
+                    width: 90,
+                    shadowColor: "white",
+                    alignContent: "center",
+                    alignItems: "center",
+                    borderColor: "grey",
+                    height: 30,
+                    justifyContent: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: "#F2F2F2",
+                      fontWeight: "500",
+                      fontSize: 14,
+                    }}
+                  >
+                    Close
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Background>
+        </Header>
+      </Modal>
+    </Header>
   );
 };
 
@@ -105,7 +240,7 @@ const styles = StyleSheet.create({
   },
   card: {
     alignSelf: "center",
-    justifyContent:'space-between',
+    justifyContent: "space-between",
     backgroundColor: "#B6A2CF",
     borderRadius: 10,
     padding: 20,
@@ -119,24 +254,46 @@ const styles = StyleSheet.create({
     // flexDirection: "row",
     marginBottom: 10,
   },
+  card2: {
+    alignSelf: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#B6A2CF",
+    borderRadius: 40,
+    opacity:0.7,
+    padding: 20,
+    width: "90%", 
+    height:'20%',
+    shadowColor: "#000",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 2.84,
+    elevation: 5, // For iOS
+    marginTop: 60,
+    // flexDirection: "row",
+    marginBottom: 60,
+  },
+  modalContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
   header: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    textAlign: 'center',
-    color:'white'
+    textAlign: "center",
+    color: "white",
   },
   questionContainer: {
     marginBottom: 20,
   },
   questionText: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   optionContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 5,
   },
   selectedOption: {
@@ -144,24 +301,37 @@ const styles = StyleSheet.create({
   },
   optionText: {
     flex: 1,
-    textAlign: 'left',
+    textAlign: "left",
     marginLeft: 10,
   },
   button: {
     marginTop: 30,
-   backgroundColor:'#B6A2CF'
+    backgroundColor: "#B6A2CF",
   },
   averageText: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 20,
-    color:'white'
+    color: "white",
   },
   result: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 20,
-    color:'#fdc500'
+    color: "#fdc500",
+  },
+  text: {
+    marginTop: 20,
+    fontSize: 15,
+    marginLeft:5,
+    fontWeight: "500",
+    color: "white",
+  },
+  text2: {
+    marginTop: 20,
+    fontSize: 15,
+    fontWeight: "700",
+    color: "black",
   },
 });
 
